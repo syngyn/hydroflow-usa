3bffd3819b360746f70b097eb5b38419d03edfd5
Jason Rusk <jason.rusk@hydroflow-usa.com>
2025-12-19 20:15:50 +0000

    File changes

--- src/components/SignalIcon.jsx
+++ src/components/SignalIcon.jsx
@@ -9,11 +9,12 @@ export default function SignalIcon({ className = "w-6 h-6" }) {
       className={className}
     >
       <path 
-        d="M2 12C2 12 4 8 6 8C8 8 8 16 10 16C12 16 12 8 14 8C16 8 16 16 18 16C20 16 20 8 22 8" 
+        d="M2 12 Q3 6 4 12 T6 12 Q7 10 8 12 T10 12 Q11 11 12 12 T14 12 Q15 11.5 16 12 T18 12 Q19 11.7 20 12 T22 12" 
         stroke="currentColor" 
         strokeWidth="2" 
         strokeLinecap="round" 
         strokeLinejoin="round"
+        fill="none"
       />
     </svg>
   );
