96e48df7ec3aa035096518955eef05f8a6f4e4b4
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-01-15 18:51:57 +0000

    File changes

--- src/pages/Warranty.jsx
+++ src/pages/Warranty.jsx
@@ -20,9 +20,6 @@ export default function Warranty() {
             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-6">
               Warranty Information
             </h1>
-            <p className="text-xl text-slate-300">
-              Five-Year Limited Warranty on <i>Hydro</i>FLOW Products
-            </p>
           </motion.div>
         </div>
       </section>
