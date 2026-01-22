6d9a01064d805937aa211d4c8dc1d94d5bc54663
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-01-22 20:41:04 +0000

    HydroFLOW branding update

--- src/pages.config.js
+++ src/pages.config.js
@@ -33,7 +33,6 @@ import Technology from './pages/Technology';
 import TermsOfService from './pages/TermsOfService';
 import Warranty from './pages/Warranty';
 import WaterHardnessMap from './pages/WaterHardnessMap';
-import ProductSelector from './pages/ProductSelector';
 import __Layout from './Layout.jsx';
 
 
@@ -73,7 +72,6 @@ export const PAGES = {
     "TermsOfService": TermsOfService,
     "Warranty": Warranty,
     "WaterHardnessMap": WaterHardnessMap,
-    "ProductSelector": ProductSelector,
 }
 
 export const pagesConfig = {
