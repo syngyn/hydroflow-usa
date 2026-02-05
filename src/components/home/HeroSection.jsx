5007b1ebda46638b7fd19b1be89be6908e97c8eb
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-05 19:09:04 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -160,7 +160,8 @@ export default function HeroSection() {
                         alt={products[currentProductIndex].name}
                         className="max-h-full w-auto object-contain"
                         style={
-                          currentProductIndex === 2 ? { transform: 'scale(0.7)' } : {}
+                          currentProductIndex === 2 ? { transform: 'scale(0.7)' } : 
+                          currentProductIndex === 4 ? { transform: 'scale(0.9)' } : {}
                         }
                       />
                     </div>
