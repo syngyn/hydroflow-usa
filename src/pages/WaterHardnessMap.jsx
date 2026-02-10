78cd7c8763a29a3be4e39acbc5588c1d51bc9dbd
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-10 20:17:29 +0000

    File changes

--- src/pages/WaterHardnessMap.jsx
+++ src/pages/WaterHardnessMap.jsx
@@ -58,7 +58,7 @@ export default function WaterHardnessMap() {
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            className="mb-8"
+            className="mb-8 lg:max-w-md"
           >
             <LocationSearch onLocationSelect={setSelectedLocation} />
           </motion.div>
