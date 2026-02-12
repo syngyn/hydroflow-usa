c4e2b75ea9f8af4dd2f277e1d5314d8b327b02a7
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-12 17:13:57 +0000

    File changes

--- src/components/water-map/LocationSearch.jsx
+++ src/components/water-map/LocationSearch.jsx
@@ -122,6 +122,8 @@ export default function LocationSearch({ onLocationSelect }) {
               </div>
             </button>
           ))}
+          </>
+          )}
         </motion.div>
       )}
     </div>
