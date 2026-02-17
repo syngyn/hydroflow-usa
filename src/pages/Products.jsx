504ce97303b552e94b5b96f914eea694c266a1a9
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-17 22:04:00 +0000

    File changes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -276,7 +276,8 @@ export default function Products() {
             </h2>
           </motion.div>
 
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+          <div className="flex justify-center">
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
             {products.filter(p => p.category === 'residential').map((product, index) => (
               <motion.div
                 key={product.id}
