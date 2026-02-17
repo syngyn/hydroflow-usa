16b34069394f2874355c5373847c27cf980c10a0
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-17 21:00:55 +0000

    File changes

--- src/pages/ProductIRange.jsx
+++ src/pages/ProductIRange.jsx
@@ -294,6 +294,8 @@ export default function ProductIRange() {
                           window.open('https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/b411d45c8_HydroFLOW-i-Range-Gen-4-Specifications.pdf', '_blank');
                         } else if (doc === '<i>Hydro</i>FLOW General Brochure') {
                           window.open('https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/2484cae7b_Hydropath_Technology-HydroFLOW_General_Brochure.pdf', '_blank');
+                        } else if (doc === '<i>Hydro</i>FLOW Commercial Brochure') {
+                          window.open('https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/769ba1377_Hydropath_Technology-HydroFLOW_Commercial_Brochure.pdf', '_blank');
                         }
                       }}
                     >
