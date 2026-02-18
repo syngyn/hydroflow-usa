ef1896f9880e902502e41f1823bacb49ac535a1c
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-18 21:49:34 +0000

    File changes

--- functions/createStripeCheckout.ts
+++ functions/createStripeCheckout.ts
@@ -91,7 +91,7 @@ Deno.serve(async (req) => {
       line_items: lineItems,
       mode: 'payment',
       success_url: `${origin}/CheckoutSuccess?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${origin}/checkout`,
+      cancel_url: `${origin}/Checkout`,
       customer_email: customerEmail,
       phone_number_collection: { enabled: true },
       metadata: {
