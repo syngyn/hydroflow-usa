81668f1d77fda9dae673a2828443c83faf81356f
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-26 15:41:47 +0000

    State Pages Footer Link

--- src/components/state-water-hardness/StateWaterHardnessTemplate.jsx
+++ src/components/state-water-hardness/StateWaterHardnessTemplate.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 import { motion } from 'framer-motion';
 
 export default function StateWaterHardnessTemplate({ 
--- src/pages/StateWaterHardness.jsx
+++ src/pages/StateWaterHardness.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { motion } from 'framer-motion';
