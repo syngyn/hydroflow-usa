a6f142b68ac87187d636095da089a777fe771bb9
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-26 15:35:51 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import Navbar from '@/components/navigation/Navbar';
-import Footer from '@/components/navigation/Footer';
-import { CartProvider } from '@/components/cart/CartContext';
-import { RecommendationProvider } from '@/components/recommendations/RecommendationContext';
-import { Toaster } from 'sonner';
+import { HelmetProvider } from 'react-helmet-async';
+import Header from './components/navigation/Header';
+import Footer from './components/navigation/Footer';
 
 export default function Layout({ children, currentPageName }) {
   return (
-    <CartProvider>
-      <RecommendationProvider>
-        <Helmet>
-          <title>HydroFLOW USA - Eco-Friendly Water Treatment Solutions</title>
-          <meta name="description" content="HydroFLOW offers innovative, chemical-free water treatment technology for residential, commercial, and industrial applications. Prevent scale and biofilm naturally." />
-        </Helmet>
-        <div className="min-h-screen flex flex-col bg-white">
-          <Navbar />
-          <main className="flex-grow">
-            {children}
-          </main>
-          <Footer currentPageName={currentPageName} />
-          <Toaster position="top-right" richColors />
-        </div>
-      </RecommendationProvider>
-    </CartProvider>
+    <HelmetProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header currentPageName={currentPageName} />
+        <main className="flex-grow">
+          {children}
+        </main>
+        <Footer currentPageName={currentPageName} />
+      </div>
+    </HelmetProvider>
   );
 }
