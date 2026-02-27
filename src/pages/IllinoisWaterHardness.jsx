6eeb27f752fc0bd84060e217da3cf000f9eb6743
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-02-27 17:36:39 +0000

    File changes

--- src/pages/IllinoisWaterHardness.jsx
+++ src/pages/IllinoisWaterHardness.jsx
@@ -43,7 +43,7 @@ export default function IllinoisWaterHardness() {
               animate={{ opacity: 1, y: 0 }}
               className="text-center"
             >
-              <h1 className="text-5xl md:text-6xl font-bold text-white mb-6">Illinois</h1>
+              <h1 className="text-5xl md:text-6xl font-bold text-white mb-6" style={{textShadow: '-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000'}}>Illinois</h1>
             </motion.div>
           </div>
         </section>
