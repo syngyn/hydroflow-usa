9ff88ae6455aa488a6b5a16d49b03b38478f10bc
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-02 16:17:37 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -12,7 +12,7 @@ const products = [
     coverage: 'For homes up to 3,000 sq ft',
     oldPrice: '$695',
     price: '$495',
-    image: 'https://hydroflow-usa.com/wp-content/uploads/2023/08/pearl-plus-transparent.webp',
+    image: 'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/00e27e7ff_pearl-plus-on-pipe.png',
     page: 'ProductPearlPlus'
   },
   {
