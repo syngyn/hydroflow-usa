2fb4c3bc80dc6a5c3c84f4b76856cc3f3436a5a3
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-03 21:37:14 +0000

    File changes

--- src/pages/CaseStudyDetail.jsx
+++ src/pages/CaseStudyDetail.jsx
@@ -956,7 +956,7 @@ const caseStudyData = {
   'car-carrier-main-cooling-system': {
     category: 'marine',
     title: 'Car Carrier - Main Cooling System',
-    image: 'https://hydroflow-usa.com/wp-content/uploads/2023/11/42481495666712-510x510.jpg',
+    image: 'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/646847a63_42481495666712.jpg',
     application: 'Car carrier vessel cooling system',
     installedUnits: 'HydroFLOW HM Range',
     results: [
