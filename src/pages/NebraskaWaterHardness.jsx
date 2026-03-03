86391c71a930db4b85fedead4eb12ba592718c6b
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-03 18:40:33 +0000

    Mississippi Riverboat Image

--- src/pages/NebraskaWaterHardness.jsx
+++ src/pages/NebraskaWaterHardness.jsx
@@ -27,7 +27,7 @@ export default function NebraskaWaterHardness() {
         <section className="relative pt-32 pb-20">
           <div className="absolute inset-0">
             <img 
-              src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/8022c6c10_12019-nebraska-321291_1920.jpg" 
+              src="https://water.usgs.gov/edu/images/characteristics/hard-water-map.jpg" 
               alt="Nebraska Water Hardness Map" 
               className="w-full h-full object-cover"
             />
