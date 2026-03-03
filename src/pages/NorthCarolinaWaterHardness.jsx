565f352c59aeb81f18653c7d1a9039e482a0e833
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-03 18:56:15 +0000

    File changes

--- src/pages/NorthCarolinaWaterHardness.jsx
+++ src/pages/NorthCarolinaWaterHardness.jsx
@@ -28,7 +28,7 @@ export default function NorthCarolinaWaterHardness() {
         <section className="relative pt-32 pb-20">
           <div className="absolute inset-0">
             <img 
-              src="https://water.usgs.gov/edu/images/characteristics/hard-water-map.jpg" 
+              src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/821ec2e4f_12019-charlotte-69203_1920.jpg" 
               alt="North Carolina Water Hardness Map" 
               className="w-full h-full object-cover"
             />
