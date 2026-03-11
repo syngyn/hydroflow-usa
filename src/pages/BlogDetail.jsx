81c83cc186fccf4c002b87e10ed7e144fd4516e7
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-11 18:34:02 +0000

    File changes

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -2009,7 +2009,7 @@ const blogContent = {
     content: `
       <p>The Somersworth, NH, Wastewater Treatment Plant serves a city with a population of approximately 11,800 people. A city-wide collection system delivers the citizens' and local industry's wastewater to be treated at the 2.4-million-gpd facility, which utilizes a GEA Westfalia Separator, model CB 505-00-32 centrifuge in its sludge dewatering process. The plant's management team agreed to evaluate Hydropath technology's ability to reduce polymer usage in this process.</p>
       
-      <img src="https://hydroflow-usa.com/assets/userfiles/images/Blog%2B4%2B-%2BPic%2B1(1).png" alt="Somersworth, NH, Wastewater Treatment Plant" />
+      <img src="https://media.base44.com/images/public/6933444aa399ff1da59bbd5c/257ee966a_Blog4-Pic11.png" alt="Somersworth, NH, Wastewater Treatment Plant" />
       
       <h2>Reduce Struvite in Wastewater with HydroFLOW</h2>
       <p><i>Hydro</i>FLOW devices have been installed as a means to control struvite accumulation in wastewater plants. In the fall of 2013, equipment was installed at the Orlando, FL, Water Conserv II Water Reclamation Facility for struvite removal and prevention purposes. After five months, the results showed significant reduction of struvite buildup and a 20% reduction in polymer use. This polymer reduction discovery led to further investigation of the phenomenon.</p>
