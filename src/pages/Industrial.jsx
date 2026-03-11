f3ce57dc512036823c5aa8cfa5d913d1c4f33589
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-11 16:10:53 +0000

    File changes

--- src/pages/Industrial.jsx
+++ src/pages/Industrial.jsx
@@ -298,8 +298,8 @@ export default function Industrial() {
             >
               <div className="mb-6">
                 <img 
-                  src="https://hydroflow-usa.com/wp-content/uploads/2025/10/irange1.png"
-                  alt="HydroFLOW I Range"
+                  src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/6933444aa399ff1da59bbd5c/9f1053a47_irange1.png"
+                  alt="<i>Hydro</i>FLOW <i>i</i> Range"
                   className="w-full max-w-xs mx-auto"
                 />
               </div>
