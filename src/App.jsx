f206fb5ca3df9934303b7e2684b1420219dfdad9
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-12 21:25:05 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import AssetsFilesRedirect from './pages/AssetsFilesRedirect';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
