4cce244416dacc2fcb0cbb6d61add7f8c1e7262f
Jason Rusk <jason.rusk@hydroflow-usa.com>
2026-03-13 15:30:01 +0000

    Manual code change

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,10 @@ export default defineConfig({
     base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
       // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true'
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
+      hmrNotifier: true,
+      navigationNotifier: true,
+      visualEditAgent: true
     }),
     react(),
   ]
